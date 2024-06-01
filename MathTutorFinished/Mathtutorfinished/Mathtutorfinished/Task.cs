deecfccd25bba45579d0a42c67c763cb58dea49e
Lebedev Boris <lebedevb144@gmail.com>
2024-06-01 16:35:44 +0300

    Did TaskB2

--- MathTutorFinished/Mathtutorfinished/Mathtutorfinished/Task.cs
+++ MathTutorFinished/Mathtutorfinished/Mathtutorfinished/Task.cs
@@ -1,10 +1,8 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace MathTutor
 {
@@ -14,6 +12,7 @@ namespace MathTutor
         public string Question { get; private set; }
         public string RightAnswer { get; private set; }
         public string Hint { get; private set; }
+
         public Task(string question, string rightAnswer)
         {
             Question = question;
@@ -24,6 +23,43 @@ namespace MathTutor
         public override string ToString()
         {
             return $"{Question}\n{RightAnswer}\n";
+        }
+    }
+
+    // Класс для задания с выбором ответа
+    public class MultipleChoiceTask : Task
+    {
+        public List<string> Answers { get; private set; }
+
+        public MultipleChoiceTask(string question, string rightAnswer, List<string> answers)
+            : base(question, rightAnswer)
+        {
+            Answers = answers;
+            Hint = GetRandomIncorrectAnswer();
+        }
+
+        private string GetRandomIncorrectAnswer()
+        {
+            var incorrectAnswers = Answers.Where(a => a != RightAnswer).ToList();
+            var random = new Random();
+            return incorrectAnswers[random.Next(incorrectAnswers.Count)];
+        }
+
+        public void ShuffleAnswers()
+        {
+            var random = new Random();
+            Answers = Answers.OrderBy(a => random.Next()).ToList();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Question);
+            foreach (var answer in Answers)
+            {
+                sb.AppendLine(answer);
+            }
+            return sb.ToString();
         }
     }
 }
